ac751af6612b55a91b3084e54d56d73fd4e3137c
Fernando García <jfer.garciav@gmail.com>
2018-05-31 05:55:42 -0500

    Change mysql to postgres

--- instalg/settings.py
+++ instalg/settings.py
@@ -1,5 +1,5 @@
 import os
-import pymysql; pymysql.install_as_MySQLdb() # Se instala el driver para conectar a MySQL
+# import pymysql; pymysql.install_as_MySQLdb() # Se instala el driver para conectar a MySQL
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
